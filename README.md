6691e8723603f3d72a798c00b48deff6f6205910
taylorarbour <77947257+taylorarbour@users.noreply.github.com>
2021-02-07 20:57:00 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # dig4639-ex2
+
+“Don’t Let Yesterday Take Up Too Much Of Today.” – Will Rogers
---
+++ index.js
@@ -0,0 +1,8 @@
+let quotes = ['“Don’t Let Yesterday Take Up Too Much Of Today.” – Will Rogers',
+'“Be the person your dog thinks you are!” - J.W. Stephens',
+'“You can’t just give up. Is that what a dinosaur would do?” - Joey Tribbiani',
+]
+
+let index = (Math.floor(Math.random() * 2))
+
+console.log(quotes[index])
